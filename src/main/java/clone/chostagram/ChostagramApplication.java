b2707891b694715f2568ebb7c715915cc95a6b01
jjdone <ehdlfaos@gmail.com>
2023-01-25 14:18:33 +0900

    init commit

---
+++ src/main/java/clone/chostagram/ChostagramApplication.java
@@ -0,0 +1,13 @@
+package clone.chostagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChostagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChostagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clone/chostagram/ChostagramApplicationTests.java
@@ -0,0 +1,13 @@
+package clone.chostagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChostagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
