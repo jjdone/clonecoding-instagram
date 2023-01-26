80ddd65ebbd89693923b5e1e880b460cb05c6d36
jjdone <ehdlfaos@gmail.com>
2023-01-26 19:11:31 +0900

    Add static and templates And simply implement IndexController

--- src/main/java/clone/chostagram/web/controller/IndexController.java
+++ src/main/java/clone/chostagram/web/controller/IndexController.java
@@ -1,2 +1,13 @@
-package clone.chostagram.web.controller;public class IndexController {
+package clone.chostagram.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
