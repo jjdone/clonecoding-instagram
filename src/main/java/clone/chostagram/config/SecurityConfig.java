7ca2d34664e4a27ce7f774e6628e97a983255da3
jjdone <ehdlfaos@gmail.com>
2023-01-27 11:31:34 +0900

    SecurityConfig 설정

---
+++ src/main/java/clone/chostagram/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package clone.chostagram.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        http.authorizeRequests()
+//                .antMatchers("/", "/login", "/singup", "/style/**", "/js/**", "/img/**").permitAll()
+//                .anyRequest().authenticated();
+//    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/singup", "/style/**", "/js/**", "/img/**").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
