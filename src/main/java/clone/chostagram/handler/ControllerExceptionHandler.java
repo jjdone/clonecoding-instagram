f7b108b785cbefc1912dd293b6d5730aa45240c9
jjdone <ehdlfaos@gmail.com>
2023-01-30 17:28:16 +0900

    유효성 검사 구현

--- src/main/java/clone/chostagram/handler/ControllerExceptionHandler.java
+++ src/main/java/clone/chostagram/handler/ControllerExceptionHandler.java
@@ -1,2 +1,35 @@
-package clone.chostagram.handler;public class ControllerExceptionHandler {
+package clone.chostagram.handler;
+
+import clone.chostagram.handler.exception.CustomApiException;
+import clone.chostagram.handler.exception.CustomValidationApiException;
+import clone.chostagram.handler.exception.CustomValidationException;
+import clone.chostagram.util.Script;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(CustomValidationException.class)
+    public String validationException(CustomValidationException e) {
+        if (e.getErrorMap() == null) {
+            return Script.back(e.getMessage());
+        } else {
+            return Script.back(e.getErrorMap().toString());
+        }
+    }
+
+    @ExceptionHandler(CustomValidationApiException.class)
+    public ResponseEntity<?> validationApiException(CustomValidationApiException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<?> apiException(CustomApiException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/clone/chostagram/handler/exception/CustomApiException.java
+++ src/main/java/clone/chostagram/handler/exception/CustomApiException.java
@@ -1,2 +1,8 @@
-package clone.chostagram.handler.exception;public class CustomApiException {
+package clone.chostagram.handler.exception;
+
+public class CustomApiException extends RuntimeException{
+
+    public CustomApiException(String message) {
+        super(message);
+    }
 }
