ff50afc5e4cf8b1e408f3c8c419fdb18eb604ef6
jjdone <ehdlfaos@gmail.com>
2023-02-01 10:20:58 +0900

    Api 유효성 검사

--- src/main/java/clone/chostagram/handler/aop/ValidationAdvice.java
+++ src/main/java/clone/chostagram/handler/aop/ValidationAdvice.java
@@ -1,2 +1,60 @@
-package clone.chostagram.handler.aop;public class ValidationAdvice {
+package clone.chostagram.handler.aop;
+
+import clone.chostagram.handler.exception.CustomValidationApiException;
+import clone.chostagram.handler.exception.CustomValidationException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Aspect
+public class ValidationAdvice {
+
+    @Around("execution(* clone.chostagram.web.controller.api.*Controller.*(..))\")")
+    public Object apiAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        for (Object arg : args) {
+            if (arg instanceof BindingResult) {
+                BindingResult bindingResult = (BindingResult) arg;
+
+                if (bindingResult.hasErrors()) {
+                    Map<String, String> errorMap = new HashMap<>();
+
+                    for (FieldError error : bindingResult.getFieldErrors()) {
+                        errorMap.put(error.getField(), error.getDefaultMessage());
+                    }
+                    throw new CustomValidationApiException("유효성 검사 실패", errorMap);
+                }
+            }
+        }
+        return proceedingJoinPoint.proceed();
+    }
+
+    @Around("execution(* clone.chostagram.web.controller.*Controller.*(..))")
+    public Object advice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        for (Object arg : args) {
+            if (arg instanceof BindingResult) {
+                BindingResult bindingResult = (BindingResult) arg;
+
+                if (bindingResult.hasErrors()) {
+                    Map<String, String> errorMap = new HashMap<>();
+
+                    for (FieldError error : bindingResult.getFieldErrors()) {
+                        errorMap.put(error.getField(), error.getDefaultMessage());
+                    }
+                    throw new CustomValidationException("유효성 검사 실패", errorMap);
+                }
+            }
+        }
+        return proceedingJoinPoint.proceed();
+    }
 }
